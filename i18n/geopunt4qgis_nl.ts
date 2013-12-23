c9e8a86cf8d87b9420ace20f2bf2a83421494418
Kay Warrie <kaywarrie@gmail.com>
2013-12-23 09:40:22 +0100

    readded ts-files

---
+++ i18n/geopunt4qgis_nl.ts
@@ -0,0 +1,173 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>aboutDlg</name>
+    <message>
+        <location filename="../ui_geopunt4QgisAbout.ui" line="14"/>
+        <source>Over Geopunt4QGIS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_geopunt4QgisAbout.ui" line="35"/>
+        <source>images</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_geopunt4QgisAbout.ui" line="36"/>
+        <source>../images</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>geopunt4Qgis</name>
+    <message>
+        <location filename="../ui_geopunt4qgis.ui" line="15"/>
+        <source>Zoek een adres via Geopunt </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_geopunt4qgis.ui" line="25"/>
+        <source>Voer een adres in en selecteer een gemeente</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_geopunt4qgis.ui" line="31"/>
+        <source>straat huisnummer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_geopunt4qgis.ui" line="67"/>
+        <source>Toevoegen aan Kaart</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_geopunt4qgis.ui" line="74"/>
+        <source>Zoom naar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../geopunt4qgis.py" line="125"/>
+        <source>Zoek een Adres: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../geopunt4qgis.py" line="125"/>
+        <source>Klik op de kaart om het adres op te vragen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../geopunt4qgis.py" line="149"/>
+        <source>Resultaat: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../geopunt4qgis.py" line="165"/>
+        <source>Waarschuwing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../geopunt4qgis.py" line="159"/>
+        <source>Geen resultaten gevonden</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../geopunt4qgis.py" line="169"/>
+        <source>onbekende fout</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>geopunt4QgisAdresDialog</name>
+    <message>
+        <location filename="../geopunt4qgisAdresdialog.py" line="155"/>
+        <source>Waarschuwing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../geopunt4qgisAdresdialog.py" line="159"/>
+        <source>onbekende fout</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>geopunt4QgisPoiDlg</name>
+    <message>
+        <location filename="../ui_geopunt4QgisPoi.ui" line="18"/>
+        <source>Zoek een Locatie via geopunt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_geopunt4QgisPoi.ui" line="31"/>
+        <source>Zoek naar een plaats op naam:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_geopunt4QgisPoi.ui" line="59"/>
+        <source>Beperk zoekresultaten tot huidige extent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_geopunt4QgisPoi.ui" line="100"/>
+        <source>id</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_geopunt4QgisPoi.ui" line="105"/>
+        <source>Categorie</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_geopunt4QgisPoi.ui" line="110"/>
+        <source>Naam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_geopunt4QgisPoi.ui" line="115"/>
+        <source>crab adres</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_geopunt4QgisPoi.ui" line="168"/>
+        <source>Voeg selectie toe aan kaart</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_geopunt4QgisPoi.ui" line="146"/>
+        <source>Zoom naar selectie</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui_geopunt4QgisPoi.ui" line="163"/>
+        <source>Zoom naar Selectie</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>geopunt4QgisPoidialog</name>
+    <message>
+        <location filename="../geopunt4QgisPoidialog.py" line="113"/>
+        <source>Geen resultaten gevonden voor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../geopunt4QgisPoidialog.py" line="117"/>
+        <source>Waarschuwing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../geopunt4QgisPoidialog.py" line="121"/>
+        <source>onbekende fout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../geopunt4QgisPoidialog.py" line="128"/>
+        <source>Merk op</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../geopunt4QgisPoidialog.py" line="128"/>
+        <source>Er niets om naar te zoomen</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
